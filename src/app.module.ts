bfba04b0f22042de0a9142db095d8238df61f2c0
Alexander Borovskikh <borislav_god@mail.ru>
2026-05-22 15:25:15 +0500

    Создан модуль авторизации

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
 import { PrismaModule } from './services/prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-	imports: [PrismaModule]
+	imports: [PrismaModule, AuthModule]
 })
 export class AppModule {}
