b32f0956fb6dbc38cc41ac906b99362caea80585
Alexander (AlexDevCo) Borovskikh <borislav_god@mail.ru>
2026-09-02 19:47:27 +0500

    Добавлено создание информации для БД в Seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,7 @@
 import { NotFoundException } from '@nestjs/common'
 import { hash } from 'argon2'
 
-import { Role } from './generated/enums'
+import { Role, SubsystemType } from './generated/enums'
 import { prisma } from './prisma-client'
 
 async function makeOrganization() {
@@ -330,18 +330,438 @@ async function makeEmployee() {
 async function makeSystem() {
 	await prisma.system.create({
 		data: {
-			code: '',
-			name: '',
-			prefix: '',
+			code: 'REA',
+			name: 'Система РосЭнергоАтом (EED->EEQ->EEP)',
+			prefix: 'ZEA',
+			subsystems: {
+				createMany: {
+					data: [
+						{
+							code: 'EED',
+							name: 'Система РосЭнергоАтом (EED)',
+							type: SubsystemType.DEVELOPMENT,
+							mandants: ['200']
+						},
+						{
+							code: 'EEP',
+							name: 'Система РосЭнергоАтом (EEP)',
+							type: SubsystemType.PRODUCTION,
+							mandants: ['400']
+						},
+						{
+							code: 'EEQ',
+							name: 'Система РосЭнергоАтом (EEQ)',
+							type: SubsystemType.TEST,
+							mandants: ['300', '320', '360']
+						},
+						{
+							code: 'BR2',
+							name: 'Система РосЭнергоАтом (BR2)',
+							type: SubsystemType.PRODUCTION_COPY,
+							mandants: ['400']
+						}
+					]
+				}
+			}
 		}
 	})
 
-	await prisma.system.createMany({
+	await prisma.system.create({
+		data: {
+			code: 'SOLM',
+			name: 'Система Solution Manager (SMD->SMQ->SMP)',
+			prefix: 'ZG',
+			subsystems: {
+				createMany: {
+					data: [
+						{
+							code: 'SMD',
+							name: 'Система Solution Manager (SMD)',
+							type: SubsystemType.DEVELOPMENT,
+							mandants: ['100']
+						},
+						{
+							code: 'SMP',
+							name: 'Система Solution Manager (SMP)',
+							type: SubsystemType.PRODUCTION,
+							mandants: ['300']
+						},
+						{
+							code: 'SMQ',
+							name: 'Система Solution Manager (SMQ)',
+							type: SubsystemType.TEST,
+							mandants: ['200']
+						}
+					]
+				}
+			}
+		}
+	})
+
+	await prisma.system.create({
+		data: {
+			code: 'ГК',
+			name: 'Система ERP для госкорпорации (RED->RET->REP)',
+			prefix: 'ZATM',
+			subsystems: {
+				createMany: {
+					data: [
+						{
+							code: 'RED',
+							name: 'Система ERP для госкорпорации (RED)',
+							type: SubsystemType.DEVELOPMENT,
+							mandants: ['100']
+						},
+						{
+							code: 'REP',
+							name: 'Система ERP для госкорпорации (REP)',
+							type: SubsystemType.PRODUCTION,
+							mandants: ['300', '320', '400', '420']
+						},
+						{
+							code: 'RET',
+							name: 'Система ERP для госкорпорации (RET)',
+							type: SubsystemType.TEST,
+							mandants: ['200', '220']
+						}
+					]
+				}
+			}
+		}
+	})
+
+	await prisma.system.create({
+		data: {
+			code: 'КС2',
+			name: 'Система ИСУП КС (NED-NEQ)',
+			prefix: 'ZKS',
+			subsystems: {
+				createMany: {
+					data: [
+						{
+							code: 'NED',
+							name: 'Система ИСУП КС (NED)',
+							type: SubsystemType.DEVELOPMENT,
+							mandants: ['100', '130']
+						},
+						{
+							code: 'NEP',
+							name: 'Система ИСУП КС (NEP)',
+							type: SubsystemType.PRODUCTION,
+							mandants: ['300']
+						},
+						{
+							code: 'NEQ',
+							name: 'Система ИСУП КС (NEQ)',
+							type: SubsystemType.TEST,
+							mandants: ['210', '230', '240']
+						},
+						{
+							code: 'NP2',
+							name: 'Система ИСУП КС (NP2)',
+							type: SubsystemType.PRODUCTION_COPY,
+							mandants: ['300']
+						}
+					]
+				}
+			}
+		}
+	})
+
+	await prisma.system.create({
+		data: {
+			code: 'КФШ',
+			name: 'Мастер-система проекта КФШ (FED->FET)',
+			prefix: 'ZFT',
+			subsystems: {
+				createMany: {
+					data: [
+						{
+							code: 'FED',
+							name: 'Мастер-система проекта КФШ (FED)',
+							type: SubsystemType.DEVELOPMENT,
+							mandants: ['100']
+						}
+					]
+				}
+			}
+		}
+	})
+
+	await prisma.system.create({
+		data: {
+			code: 'КФШ2',
+			name: 'Система S4 Hana',
+			prefix: 'ZFT2',
+			subsystems: {
+				createMany: {
+					data: [
+						{
+							code: 'HED',
+							name: 'Система S4 Hana (HED)',
+							type: SubsystemType.DEVELOPMENT,
+							mandants: ['100']
+						}
+					]
+				}
+			}
+		}
+	})
+
+	await prisma.system.create({
+		data: {
+			code: 'ТВЭЛ',
+			name: 'Система ERP для ТВЭЛ-МСЗ  (TED->TEQ->TEP)',
+			prefix: 'ZTK',
+			subsystems: {
+				createMany: {
+					data: [
+						{
+							code: 'TED',
+							name: 'Система ERP для ТВЭЛ-МСЗ (TED)',
+							type: SubsystemType.DEVELOPMENT,
+							mandants: ['100', '125', '400']
+						},
+						{
+							code: 'TEP',
+							name: 'Система ERP для ТВЭЛ-МСЗ (TEP)',
+							type: SubsystemType.PRODUCTION,
+							mandants: ['300']
+						},
+						{
+							code: 'TEQ',
+							name: 'Система ERP для ТВЭЛ-МСЗ (TEQ)',
+							type: SubsystemType.TEST,
+							mandants: ['200', '220', '225', '240']
+						}
+					]
+				}
+			}
+		}
+	})
+
+	await prisma.system.create({
+		data: {
+			code: 'ТСЭ',
+			name: 'Система ERP для Техснабэкспорт (SED -> SEQ -> SEP)',
+			prefix: 'ZTS',
+			subsystems: {
+				createMany: {
+					data: [
+						{
+							code: 'SED',
+							name: 'Система ERP для Техснабэкспорт (SED)',
+							type: SubsystemType.DEVELOPMENT,
+							mandants: ['100']
+						},
+						{
+							code: 'SEP',
+							name: 'Система ERP для Техснабэкспорт (SEP)',
+							type: SubsystemType.PRODUCTION,
+							mandants: ['300']
+						},
+						{
+							code: 'SEQ',
+							name: 'Система ERP для Техснабэкспорт (SEQ)',
+							type: SubsystemType.TEST,
+							mandants: ['200', '210', '220', '230']
+						}
+					]
+				}
+			}
+		}
+	})
+}
+
+async function makeModule() {
+	await prisma.module.createMany({
 		data: [
 			{
-				code: '',
-				name: '',
-				prefix: '',
+				code: 'BC',
+				name: 'Базис'
+			},
+			{
+				code: 'CA',
+				name: 'Компоненты, общие для всех приложений'
+			},
+			{
+				code: 'CO',
+				name: 'Контролинг'
+			},
+			{
+				code: 'DMS',
+				name: 'Управление документами'
+			},
+			{
+				code: 'FI',
+				name: 'Финансы'
+			},
+			{
+				code: 'FM',
+				name: 'Бюджет'
+			},
+			{
+				code: 'HR',
+				name: 'Кадры'
+			},
+			{
+				code: 'LIB',
+				name: 'Технические библиотеки'
+			},
+			{
+				code: 'MM',
+				name: 'Материалы'
+			},
+			{
+				code: 'NSI',
+				name: 'Нормативно-справочная информация (NSI)'
+			},
+			{
+				code: 'PM',
+				name: 'ТОРО'
+			},
+			{
+				code: 'PP',
+				name: 'Управление производством'
+			},
+			{
+				code: 'PS',
+				name: 'Системы проектов'
+			},
+			{
+				code: 'QM',
+				name: 'Управление качеством'
+			},
+			{
+				code: 'RCM',
+				name: 'Case and Records Management'
+			},
+			{
+				code: 'RE',
+				name: 'Недвижимость'
+			},
+			{
+				code: 'SD',
+				name: 'Сбыт'
+			},
+			{
+				code: 'SM',
+				name: 'Разработки для Solution Manager'
+			},
+			{
+				code: 'SRM',
+				name: 'Управление взаимоотношениями с поставщиками'
+			},
+			{
+				code: 'UTL',
+				name: 'Утилиты'
+			},
+			{
+				code: 'НСИ',
+				name: 'Нормативно-справочная информация (НСИ)'
+			}
+		]
+	})
+}
+
+async function makeDevTypes() {
+	await prisma.developmentType.createMany({
+		data: [
+			{
+				code: 'BSP',
+				name: 'Приложение BSP'
+			},
+			{
+				code: 'CRD',
+				name: 'Карточка документооборота'
+			},
+			{
+				code: 'DIA',
+				name: 'Диалоговая программа'
+			},
+			{
+				code: 'DIC',
+				name: 'Изменение Словаря Данных'
+			},
+			{
+				code: 'ENH',
+				name: 'Расширение'
+			},
+			{
+				code: 'EXT',
+				name: 'Экстрактор для SAP BI (ФМ)'
+			},
+			{
+				code: 'FM',
+				name: 'Функциональный модуль'
+			},
+			{
+				code: 'FRI',
+				name: 'Fiori приложение'
+			},
+			{
+				code: 'FRM',
+				name: 'Печатная форма'
+			},
+			{
+				code: 'IFS',
+				name: 'Интерфейс'
+			},
+			{
+				code: 'INF',
+				name: 'Инфотип'
+			},
+			{
+				code: 'LIB',
+				name: 'Функциональная библиотека'
+			},
+			{
+				code: 'LSM',
+				name: 'LSMW'
+			},
+			{
+				code: 'MDL',
+				name: 'Функциональный блок'
+			},
+			{
+				code: 'NOT',
+				name: 'Имплементация ноты'
+			},
+			{
+				code: 'ODT',
+				name: 'OData сервис'
+			},
+			{
+				code: 'OTH',
+				name: 'Прочие изменения'
+			},
+			{
+				code: 'REP',
+				name: 'Отчет'
+			},
+			{
+				code: 'SRH',
+				name: 'Средство поиска'
+			},
+			{
+				code: 'SYS',
+				name: 'Модификация'
+			},
+			{
+				code: 'TLB',
+				name: 'Техническая библиотека'
+			},
+			{
+				code: 'WDP',
+				name: 'Разработка Web Dynpro'
+			},
+			{
+				code: 'WF',
+				name: 'WorkFlow'
+			},
+			{
+				code: 'ZAM',
+				name: 'Проверки, замещения, OpenFI, R'
 			}
 		]
 	})
@@ -353,6 +773,9 @@ async function up() {
 	await makePosition()
 	await makeUser()
 	await makeEmployee()
+	await makeSystem()
+	await makeModule()
+	await makeDevTypes()
 }
 
 async function down() {
@@ -362,6 +785,10 @@ async function down() {
 	await prisma.$executeRaw`TRUNCATE TABLE "positions" RESTART IDENTITY CASCADE`
 	await prisma.$executeRaw`TRUNCATE TABLE "subsidiaries" RESTART IDENTITY CASCADE`
 	await prisma.$executeRaw`TRUNCATE TABLE "organizations" RESTART IDENTITY CASCADE`
+	await prisma.$executeRaw`TRUNCATE TABLE "subsystems" RESTART IDENTITY CASCADE`
+	await prisma.$executeRaw`TRUNCATE TABLE "systems" RESTART IDENTITY CASCADE`
+	await prisma.$executeRaw`TRUNCATE TABLE "modules" RESTART IDENTITY CASCADE`
+	await prisma.$executeRaw`TRUNCATE TABLE "development_types" RESTART IDENTITY CASCADE`
 }
 
 async function main() {
