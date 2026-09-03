1fac3edce0e905c9428487be6a249a1960ae12da
Alexander (AlexDevCo) Borovskikh <borislav_god@mail.ru>
2026-09-03 17:25:22 +0500

    Добавлены экспорты классов для модуля system

--- src/v1/modules/system/system.module.ts
+++ src/v1/modules/system/system.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { SystemService } from './system.service';
-import { SystemController } from './system.controller';
+import { Module } from '@nestjs/common'
+
+import { SystemRepository } from '../../../repositories/system/system.repository'
+import { PrismaService } from '../../../services'
+
+import { SystemController } from './system.controller'
+import { SystemService } from './system.service'
 
 @Module({
-  controllers: [SystemController],
-  providers: [SystemService],
+	controllers: [SystemController],
+	providers: [SystemService, PrismaService, SystemRepository]
 })
 export class SystemModule {}
