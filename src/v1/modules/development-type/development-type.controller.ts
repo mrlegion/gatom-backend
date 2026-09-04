879882378140b46dc0bf563287f5431c96108c39
Alexander (AlexDevCo) Borovskikh <borislav_god@mail.ru>
2026-09-04 12:54:45 +0500

    Добавлен проверочный метод getAll в котроллер Development Type

--- src/v1/modules/development-type/development-type.controller.ts
+++ src/v1/modules/development-type/development-type.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
-import { DevelopmentTypeService } from './development-type.service';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common'
 
-@Controller('development-type')
+import { DevelopmentTypeService } from './development-type.service'
+import type { GetAllDevelopmentTypeResponse } from './dto'
+
+@Controller('development-types')
 export class DevelopmentTypeController {
-  constructor(private readonly developmentTypeService: DevelopmentTypeService) {}
+	constructor(
+		private readonly developmentTypeService: DevelopmentTypeService
+	) {}
+
+	@Get('')
+	@HttpCode(HttpStatus.OK)
+	public async getAll(): Promise<GetAllDevelopmentTypeResponse> {
+		return await this.developmentTypeService.getAll()
+	}
 }
