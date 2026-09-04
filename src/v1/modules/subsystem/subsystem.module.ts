f3322e4d7191d49ea0cbcc0e1fef950ba0dad53c
Alexander (AlexDevCo) Borovskikh <borislav_god@mail.ru>
2026-09-04 12:53:47 +0500

    Добавил импорты в модуль Subsystem

--- src/v1/modules/subsystem/subsystem.module.ts
+++ src/v1/modules/subsystem/subsystem.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { SubsystemService } from './subsystem.service';
-import { SubsystemController } from './subsystem.controller';
+import { Module } from '@nestjs/common'
+
+import { SubsystemRepository } from '../../../repositories/subsystem/subsystem.repository'
+import { PrismaService } from '../../../services'
+
+import { SubsystemController } from './subsystem.controller'
+import { SubsystemService } from './subsystem.service'
 
 @Module({
-  controllers: [SubsystemController],
-  providers: [SubsystemService],
+	controllers: [SubsystemController],
+	providers: [SubsystemService, SubsystemRepository, PrismaService]
 })
 export class SubsystemModule {}
