ffa301f09880a44a14ffa5dac25c22152e85c543
Alexander (AlexDevCo) Borovskikh <borislav_god@mail.ru>
2026-09-04 12:54:58 +0500

    Добавлен проверочный метод getAll в котроллер Subsystem

--- src/v1/modules/subsystem/subsystem.controller.ts
+++ src/v1/modules/subsystem/subsystem.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
-import { SubsystemService } from './subsystem.service';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common'
 
-@Controller('subsystem')
+import { GetAllSubsystemResponse } from './dto'
+import { SubsystemService } from './subsystem.service'
+
+@Controller('subsystems')
 export class SubsystemController {
-  constructor(private readonly subsystemService: SubsystemService) {}
+	constructor(private readonly subsystemService: SubsystemService) {}
+
+	@Get('')
+	@HttpCode(HttpStatus.OK)
+	public async getAll(): Promise<GetAllSubsystemResponse> {
+		return await this.subsystemService.getAll()
+	}
 }
