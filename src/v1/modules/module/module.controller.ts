222c5f074d1a4c09226ece7c4e0ad149e8e61219
Alexander (AlexDevCo) Borovskikh <borislav_god@mail.ru>
2026-09-04 12:55:06 +0500

    Добавлен проверочный метод getAll в котроллер Module

--- src/v1/modules/module/module.controller.ts
+++ src/v1/modules/module/module.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
-import { ModuleService } from './module.service';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common'
 
-@Controller('module')
+import type { GetAllModuleResponse } from './dto'
+import { ModuleService } from './module.service'
+
+@Controller('modules')
 export class ModuleController {
-  constructor(private readonly moduleService: ModuleService) {}
+	constructor(private readonly moduleService: ModuleService) {}
+
+	@Get('')
+	@HttpCode(HttpStatus.OK)
+	public async getAll(): Promise<GetAllModuleResponse> {
+		return await this.moduleService.getAll()
+	}
 }
