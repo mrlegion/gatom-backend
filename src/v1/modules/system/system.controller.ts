c7dc65453384000f56e2776ea7abfd004e7c3782
Alexander (AlexDevCo) Borovskikh <borislav_god@mail.ru>
2026-09-04 12:56:50 +0500

    Добавлен проверочный метод getAll в котроллер System

--- src/v1/modules/system/system.controller.ts
+++ src/v1/modules/system/system.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
-import { SystemService } from './system.service';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common'
+import { ApiOperation, ApiResponse } from '@nestjs/swagger'
 
-@Controller('system')
+import { GetAllSystemResponse } from './dto'
+import { SystemService } from './system.service'
+
+@Controller('systems')
 export class SystemController {
-  constructor(private readonly systemService: SystemService) {}
+	constructor(private readonly systemService: SystemService) {}
+
+	@ApiOperation({
+		summary: 'Получить все записи'
+	})
+	@ApiResponse({
+		status: HttpStatus.OK,
+		description: 'Успешное выполнение запроса',
+		type: GetAllSystemResponse
+	})
+	@Get('')
+	@HttpCode(HttpStatus.OK)
+	public async getAll(): Promise<GetAllSystemResponse> {
+		return await this.systemService.getAll()
+	}
 }
