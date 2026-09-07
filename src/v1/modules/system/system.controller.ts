6426f152ef952f020e0a20a188a5869286f8fd3f
Alexander (AlexDevCo) Borovskikh <borislav_god@mail.ru>
2026-09-07 21:46:31 +0500

    Завершил описание контроллера для модуля System

--- src/v1/modules/system/system.controller.ts
+++ src/v1/modules/system/system.controller.ts
@@ -1,10 +1,33 @@
-import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common'
-import { ApiOperation, ApiResponse } from '@nestjs/swagger'
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	HttpStatus,
+	Param,
+	Post,
+	Put
+} from '@nestjs/common'
+import {
+	ApiBearerAuth,
+	ApiBody,
+	ApiOperation,
+	ApiResponse
+} from '@nestjs/swagger'
+
+import { Auth } from '../../../shared/decorators'
+import { ErrorResponse } from '../../../shared/response'
 
 import {
+	CreateSystemRequest,
+	CreateSystemResponse,
+	DeleteSystemResponse,
 	GetAllSystemResponse,
 	SystemFindByManyResponse,
-	SystemFindByResponse
+	SystemFindByResponse,
+	UpdateSystemRequest,
+	UpdateSystemResponse
 } from './dto'
 import { SystemService } from './system.service'
 
@@ -27,8 +50,8 @@ export class SystemController {
 	})
 	@Get('')
 	@HttpCode(HttpStatus.OK)
-	public async getAll(): Promise<GetAllSystemResponse> {
-		return await this.systemService.getAll()
+	public getAll(): Promise<GetAllSystemResponse> {
+		return this.systemService.getAll()
 	}
 
 	/**
@@ -45,12 +68,10 @@ export class SystemController {
 		description: 'Успешное выполнение запроса',
 		type: SystemFindByResponse
 	})
-	@Get('/:id')
+	@Get('/find-by/id/:id')
 	@HttpCode(HttpStatus.OK)
-	public async findById(
-		@Param('id') id: string
-	): Promise<SystemFindByResponse> {
-		return await this.systemService.findById(id)
+	public findById(@Param('id') id: string): Promise<SystemFindByResponse> {
+		return this.systemService.findById(id)
 	}
 
 	/**
@@ -67,12 +88,12 @@ export class SystemController {
 		description: 'Успешное выполнение запроса',
 		type: SystemFindByResponse
 	})
-	@Get('/:code')
+	@Get('/find-by/code/:code')
 	@HttpCode(HttpStatus.OK)
-	public async findByCode(
+	public findByCode(
 		@Param('code') code: string
 	): Promise<SystemFindByResponse> {
-		return await this.systemService.findByCode(code)
+		return this.systemService.findByCode(code.toUpperCase()) // код системы всегда в верхнем регистре
 	}
 
 	/**
@@ -89,12 +110,12 @@ export class SystemController {
 		description: 'Успешное выполнение запроса',
 		type: SystemFindByResponse
 	})
-	@Get('/:name')
+	@Get('/find-by/name/:name')
 	@HttpCode(HttpStatus.OK)
-	public async findByName(
+	public findByName(
 		@Param('name') name: string
 	): Promise<SystemFindByResponse> {
-		return await this.systemService.findByName(name)
+		return this.systemService.findByName(name)
 	}
 
 	/**
@@ -108,14 +129,108 @@ export class SystemController {
 	})
 	@ApiResponse({
 		status: HttpStatus.OK,
-		description: 'Успешное выполнение запроас',
+		description: 'Успешное выполнение запроса',
 		type: SystemFindByManyResponse
 	})
-	@Get('/:prefix')
+	@Get('/find-by/prefix/:prefix')
 	@HttpCode(HttpStatus.OK)
-	public async findByPrefix(
+	public findByPrefix(
 		@Param('prefix') prefix: string
 	): Promise<SystemFindByManyResponse> {
-		return await this.systemService.findByPrefix(prefix)
+		return this.systemService.findByPrefix(prefix.toUpperCase()) // Префикс системы всегда в верхнем регистре
+	}
+
+	/**
+	 * Создание новой записи системы
+	 *
+	 * @param data Данные для создания
+	 * @returns Ответ со статусом выполнения запроса и объектом системы или Null
+	 */
+	@ApiOperation({
+		summary: 'Создание новой записи системы'
+	})
+	@ApiResponse({
+		status: HttpStatus.OK,
+		description: 'Успешное выполение запроса',
+		type: CreateSystemResponse
+	})
+	@ApiResponse({
+		status: HttpStatus.BAD_REQUEST,
+		description: 'Переданны не верные данные',
+		type: ErrorResponse
+	})
+	@ApiBody({ type: CreateSystemRequest })
+	@ApiBearerAuth()
+	@Auth()
+	@Post('')
+	@HttpCode(HttpStatus.OK)
+	public create(
+		@Body() data: CreateSystemRequest
+	): Promise<CreateSystemResponse> {
+		return this.systemService.create(data)
+	}
+
+	/**
+	 * Обновление данных системы
+	 *
+	 * @param id Уникальный идентификатор
+	 * @param data Данные для обновления
+	 * @returns Ответ со статусом выполнения запроса и объектом системы или Null
+	 */
+	@ApiOperation({
+		summary: 'Обновление данных системы'
+	})
+	@ApiResponse({
+		status: HttpStatus.OK,
+		description: 'Успешное выполнение запроса',
+		type: UpdateSystemResponse
+	})
+	@ApiResponse({
+		status: HttpStatus.NOT_FOUND,
+		description: 'Система для обновления не найдена',
+		type: ErrorResponse
+	})
+	@ApiResponse({
+		status: HttpStatus.BAD_REQUEST,
+		description: 'Переданны не верные данные',
+		type: ErrorResponse
+	})
+	@ApiBody({ type: UpdateSystemRequest })
+	@ApiBearerAuth()
+	@Auth()
+	@Put('/:id')
+	@HttpCode(HttpStatus.OK)
+	public update(
+		@Param('id') id: string,
+		@Body() data: UpdateSystemRequest
+	): Promise<UpdateSystemResponse> {
+		return this.systemService.update(id, data)
+	}
+
+	/**
+	 * Удаление записи
+	 *
+	 * @param id Уникальный идентификатор системы
+	 * @returns Ответ на запрос удалениея со статусом удаления
+	 */
+	@ApiOperation({
+		summary: 'Удаление записи'
+	})
+	@ApiResponse({
+		status: HttpStatus.OK,
+		description: 'Успешное выполнение запроса',
+		type: DeleteSystemResponse
+	})
+	@ApiResponse({
+		status: HttpStatus.NOT_FOUND,
+		description: 'Система для удаления не найдена',
+		type: ErrorResponse
+	})
+	@ApiBearerAuth()
+	@Auth()
+	@Delete('/:id')
+	@HttpCode(HttpStatus.OK)
+	public delete(@Param('id') id: string): Promise<DeleteSystemResponse> {
+		return this.systemService.delete(id)
 	}
 }
